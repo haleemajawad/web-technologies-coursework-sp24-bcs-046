2e6e942a9119c81b41298ba154396ee99a80b674
haleemajawad <haleemajawad2005@gmail.com>
2026-04-02 22:34:57 +0500

    final changes

--- assignment 2/script.js
+++ assignment 2/script.js
@@ -1,8 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
+
     const icon = document.getElementById("icon");
     const menu = document.getElementById("menu");
     const sapphirelogo = document.getElementById("sapphire-logo");
     const womenul = document.getElementById("womenul");
+    const crossmenu = document.getElementById("crossmenu");
 
     icon.addEventListener("click", function () {
         menu.classList.toggle("open");
@@ -10,4 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
         sapphirelogo.classList.toggle("disappear");
         womenul.classList.toggle("disappear");
     });
-});
+    crossmenu.addEventListener("click", function () {
+        menu.classList.remove("open");
+        console.log("Menu closed");
+        sapphirelogo.classList.remove("disappear");
+        womenul.classList.remove("disappear");
+    });
