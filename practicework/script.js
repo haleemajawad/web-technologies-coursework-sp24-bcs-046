b1c58fd179d2e9aa3c115b669e0654726748967f
haleemajawad <haleemajawad2005@gmail.com>
2026-04-04 13:52:24 +0500

    practice js basics

---
+++ practicework/script.js
@@ -0,0 +1,38 @@
+
+const hello=document.getElementsByTagName("h1");
+const button=document.getElementsByTagName("button");
+button[0].addEventListener("click",function(){
+hello[0].innerText="Button was clicked!"
+});
+
+const ul=document.getElementById("ul1");
+button[1].addEventListener("click",function(){
+const li=document.createElement("li");
+li.innerText="zainab";
+ul.appendChild(li); 
+});
+
+/*to do list */
+const ul2=document.createElement("ul");
+document.body.appendChild(ul2)
+const input=document.getElementById("input1");
+const addButton=document.getElementById("addButton");
+addButton.addEventListener("click",function(){
+
+const value=input.value;
+const div=document.createElement("div");
+const li2=document.createElement("li");
+li2.innerText=value;
+const del=document.createElement("button");
+del.innerText="deconste";
+
+div.appendChild(li2);
+div.appendChild(del);
+ul2.appendChild(div);
+
+input.value="";
+del.addEventListener("click",function(){
+    ul2.removeChild(div);
+});
+
+});
